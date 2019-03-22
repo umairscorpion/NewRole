47fe664a3e579a85cc74f9998fa4e23c4a7cc35e
ghazanfarseyal <ghazanfarseyal@gmail.com>
2019-03-22 15:30:49 +0500

    Monthly Report

--- src/app/Model/Report/report.detail.ts
+++ src/app/Model/Report/report.detail.ts
@@ -3,14 +3,15 @@ import { Entity } from "../entity";
 export class ReportDetail extends Entity {
   absenceId: number;
   employeeName: string;
+  employeeTypeTitle: string;
   absencePosition: number;
   grade: string;
   subject: string;
   reason: string;
   startDate: Date | string;
   endDate: Date | string;
-  startTime: Date | string;
-  endTime: Date | string;
+  startTime: string;
+  endTime: string;
   districtName: string;
   postedOn: Date | string;
   postedById: string;
@@ -20,5 +21,6 @@ export class ReportDetail extends Entity {
   statusDate: Date | string;
   substituteId: string;
   substituteName: string;
+  substituteProfilePicUrl: string;
   notes: string;
 }
