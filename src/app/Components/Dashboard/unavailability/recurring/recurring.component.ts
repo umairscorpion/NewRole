2846fd9a5481a3b956e297fdaad9629dca7ccbb1
ghazanfarseyal <ghazanfarseyal@gmail.com>
2019-04-03 12:37:55 +0500

    Substitute's Calendar - Recurrence Popup

---
+++ src/app/Components/Dashboard/unavailability/recurring/recurring.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import {
+  MatDialog,
+  MatDialogRef,
+  MAT_DIALOG_DATA
+} from '@angular/material';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import * as moment from 'moment';
+
+@Component({
+  selector: 'app-recurring-component',
+  templateUrl: 'recurring.component.html'
+})
+export class RecurringComponent implements OnInit {
+  availability: any;
+  submitted = false;
+  form: FormGroup;
+  constructor(
+    private dialogRef: MatDialogRef<RecurringComponent>,
+    private dialog: MatDialog,
+    private _formBuilder: FormBuilder,
+    @Inject(MAT_DIALOG_DATA) public data: any
+  ) {
+    this.availability = data;
+  }
+
+  ngOnInit() {
+    this.form = this._formBuilder.group(
+      {
+        availabilityId: [this.availability.availabilityId || 0],
+        userId: [this.availability.userId || ''],
+        availabilityStatusId: [this.availability.availabilityStatusId || 1],
+        title: [this.availability.title || '', Validators.required],
+        startDate: [this.availability.startDate, Validators.required],
+        endDate: [this.availability.endDate, Validators.required],
+        startTime: [this.availability.startTime, Validators.required],
+        endTime: [this.availability.endTime, Validators.required],
+        isAllDayOut: [this.availability.isAllDayOut || false],
+        isRepeat: [this.availability.isRepeat || false],
+        repeatType: [this.availability.repeatType || ''],
+        repeatValue: [this.availability.repeatValue || 0],
+        repeatOnWeekDays: [this.availability.repeatOnWeekDays || ''],
+        isEndsNever: [this.availability.isEndsNever || false],
+        endsOnAfterNumberOfOccurrance: [this.availability.endsOnAfterNumberOfOccurrance || 0],
+        endsOnUntilDate: [this.availability.endsOnUntilDate || ''],
+      }
+    );
+  }
+
+  onClose() {
+    this.dialogRef.close();
+  }
+
+  onSubmit(formGroup: FormGroup) {
+    this.submitted = true;
+    if (!formGroup.invalid) {
+      // this.dialogRef.close({ action: 'Submit', availability: formGroup.value });
+      // this.submitted = false;
+    }
+  }
+}
