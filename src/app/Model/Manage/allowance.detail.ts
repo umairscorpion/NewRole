0c0a56cc66736d6faccf11f1b6662fa70944343e
Taimoor <tamor1972@hotmail.com>
2019-04-05 15:49:47 +0500

    Allowance

---
+++ src/app/Model/Manage/allowance.detail.ts
@@ -0,0 +1,8 @@
+export class Allowance {
+    Id: number;
+    Title: string;
+    YearlyAllowance: string;
+    IsDeductAllowance: boolean;
+    IsResidualDays: boolean;
+    CreatedDate: Date | string;
+}
