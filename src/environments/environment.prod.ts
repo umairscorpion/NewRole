4d331f08c9399036e9276770c9e290a9e913b5e7
umairscorpion <umair@subzz.com>
2019-08-28 15:51:17 +0500

    28 August

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  // apiUrl: 'https://sapi.loginsubzz.com/api/',
-  // profileImageUrl: 'https://sapi.loginsubzz.com/Profile/'
+  //  apiUrl: 'https://sapi.loginsubzz.com/api/',
+  //  profileImageUrl: 'https://sapi.loginsubzz.com/Profile/'
   apiUrl: 'https://v1.loginsubzz.com/api/',
   profileImageUrl: 'https://v1.loginsubzz.com/Profile/'
   // apiUrl: 'http://162.241.138.178/plesk-site-preview/sapi.loginsubzz.com/api/',
